678055a7be23a8ef004232d0d25d50ef2f3fee51
Joona Lillfors <joona.lillfors@aalto.fi>
2019-07-21 11:37:58 +0300

    token to env file, /api/:username returns sum of bytes written in each language

--- project.clj
+++ project.clj
@@ -6,11 +6,13 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/algo.generic "0.1.3"]
                  [compojure "1.6.1"]
+                 [environ "1.1.0"]
                  [ring/ring-defaults "0.3.2"]
                  [http-kit "2.3.0"]
                  [clj-http "3.10.0"]
                  [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.12.5"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-pprint "1.2.0"]]
   :ring {:handler clj-git.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/clj_git/handler.clj
+++ src/clj_git/handler.clj
@@ -3,6 +3,7 @@
             [compojure.route :as route]
             [compojure.handler :as handler]
             [clojure.data.json :as json]
+            [environ.core :refer [env]]
             [org.httpkit.client :as http]
             [clj-http.client :as client]
             [ring.middleware.json :as middleware]
@@ -10,10 +11,12 @@
             [clojure.algo.generic.functor :refer [fmap]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def token (env :client-token))
+
 (defn repositories
   [name] 
   {
-    :oauth-token "9ebfb7d53950444f8d3362f29848e968d3b05ec5"
+    :oauth-token token
     :content-type :json
     :body (json/write-str {"query" "query ($name: String!) { user(login: $name) { repositories(first: 20) { nodes { name id } } } }",
                            "variables" {"name" name}})
@@ -23,7 +26,7 @@
 (defn languages
   [repos]
   {
-    :oauth-token "9ebfb7d53950444f8d3362f29848e968d3b05ec5"
+    :oauth-token token
     :content-type :json
     :body (json/write-str {"query" "query ($repos: [ID!]!) { nodes(ids: $repos) { ...on Repository { languages(first: 20) { totalSize totalCount edges { size node { name } } } } } }",
                            "variables" {"repos" repos}})
@@ -33,32 +36,53 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/:name"
+  (GET "/api/:name"
     [name]
     (do (println (json/write-str {:version "1.0", :name name}))
         (let [result (promise)]
-          (client/post "https://api.github.com/graphql" (repositories name)
-            (fn [res]
-              (let [nodes (-> (json/read-str (res :body) :key-fn keyword) :data :user :repositories :nodes)]
-                (let [repos (map #(% :id) nodes)]
-                  (println repos)
-                  (client/post "https://api.github.com/graphql" (languages repos)
-                    (fn [res]
-                      (let [node (-> (json/read-str (res :body) :key-fn keyword) :data :nodes)]
-                        ;(println node)
-                        ;(println (map #(-> % :languages :edges) node))
-                        ;calculate sizes of each language
-                        (let [langs (group-by :name (flatten (map #(vector (map (fn [edge] {:size (edge :size), :name (-> edge :node :name)}) (-> % :languages :edges))
-                        ) node)))]
-                          (println langs)
-                          (deliver result langs))
-                        ;(deliver result (map #(hash-map :languages (-> % :languages :edges)) node))
-                      ))
-                    (fn [exception]
-                      (println (.getMessage exception)))))))
-            (fn [exception]
-              (println (.getMessage exception))))
+          ;(client/with-async-connection-pool {}
+            (client/post "https://api.github.com/graphql" (repositories name)
+              (fn [resp]
+                (let [nodes (-> (json/read-str (resp :body) :key-fn keyword) :data :user :repositories :nodes)]
+                  (let [repos (map #(% :id) nodes)]
+                    (client/post "https://api.github.com/graphql" (client/reuse-pool (languages repos) resp)
+                      (fn [res]
+                        (let [node (-> (json/read-str (res :body) :key-fn keyword) :data :nodes)]
+                          (let [langs (group-by :name (flatten (map #(vector (map (fn [edge] {:size (edge :size), :name (-> edge :node :name)}) (-> % :languages :edges))
+                          ) node)))]
+                            (deliver result (fmap #(reduce (fn [sum lang] (+ (lang :size) sum)) 0 %) langs)))
+                        ))
+                      (fn [exception]
+                        (println (.getMessage exception)))))))
+              (fn [exception]
+                (println (.getMessage exception)))) ;)
           (response {:version "1.0", :name name, :body @result}))))
+    (GET "/test"
+      []
+      (let [repos (promise)
+            result (promise)]
+        (client/with-connection-pool {}
+          (let [res (client/post "https://api.github.com/graphql" {
+            :oauth-token token
+            :content-type :json
+            :body (json/write-str {"query" "query ($name: String!) { user(login: $name) { repositories(first: 20) { nodes { name id } } } }",
+                                  "variables" {"name" "joonalillfors"}})
+          })]
+            (deliver repos (map #(% :id) (-> (json/read-str (res :body) :key-fn keyword) :data :user :repositories :nodes)))
+          )
+          (let [res (client/post "https://api.github.com/graphql" {
+            :oauth-token token
+            :content-type :json
+            :body (json/write-str {"query" "query ($repos: [ID!]!) { nodes(ids: $repos) { ...on Repository { languages(first: 20) { totalSize totalCount edges { size node { name } } } } } }",
+                                  "variables" {"repos" @repos}})
+          })]
+            (let [node (-> (json/read-str (res :body) :key-fn keyword) :data :nodes)]
+              (deliver result (group-by :name (flatten (map #(vector (map (fn [edge] {:size (edge :size), :name (-> edge :node :name)}) (-> % :languages :edges))) node))))
+            )
+          )
+        )
+        (response @result)
+      ))
   (route/not-found "Not Found"))
 
 (def app
